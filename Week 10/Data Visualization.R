cc75a642aa1aa3661fce2b17ba7082358c52d8b8
Vikram S <vikram.senthil@outlook.com>
2021-01-20 13:37:30 +0530

    Create Data Visualization.R

---
+++ Week 10/Data Visualization.R
@@ -0,0 +1,97 @@
+library(ggplot2)
+library(dplyr)
+
+expenses<-read.csv("dailyexpenses.csv")
+View(expenses)
+str(expenses)
+expenses$Date <- as.Date(expenses$Date,
+                         "%d/%m/%Y")
+str(expenses)
+
+
+
+#Compare Expenses - Bar Chart 
+fd <-expenses[,2] #food column 
+a1 <- sum(fd)
+
+groc <-expenses[,3]
+a2 <- sum(groc)
+
+med <-expenses[,4]
+a3 <- sum(med)
+
+fl <-expenses[,5]
+a4 <- sum(fl)
+
+tl <-expenses[,6]
+a5 <- sum(tl)
+
+ph <-expenses[,7]
+a6 <- sum(ph)
+
+electric <- expenses[,8]
+a7 <- sum(electric)
+
+misc <- expenses[,9]
+a8 <- sum(misc)
+
+detail <- c("food", "groceries", "medical", "fuel", "toll", "phone", "electricity", "miscellaneous")
+total <- c(a1, a2, a3, a4, a5, a6, a7, a8)
+
+df <- data.frame(detail, total)
+View(df)
+
+ggplot(data = df, aes(x = detail, y = total))+
+  geom_bar(stat = "identity", fill = "purple")+
+  labs(x = "Items",
+       y = "Expenses",
+       title = "Comparing Expenses")
+
+#Total Expenses - Line chart 
+cm<-expenses[,2:9] #cumulative data
+dt<-expenses[,1]
+rs<-rowSums(cm)
+dg<-data.frame(dt,rs)
+ggplot(data = dg, aes(x = dt, y = rs))+
+  geom_line(color = "#00AFBB", size = 2)+
+  labs(x = "Date",
+       y = "Expenses",
+       title = "Total Expenses")
+
+#Pie Chart of Expenses
+pc<-read.csv("piechart.csv")
+
+View(pc)
+
+pie <-  ggplot(pc, aes(x = "", y=amount, fill = factor(expense))) +
+  geom_bar(width = 1, stat ="identity")+
+  labs(fill="expense",
+       x=NULL, y=NULL, title="Pie Chart of expenses")
+
+pie + coord_polar(theta = "y", start = 0)
+
+#medical Expenses - point graph 
+
+md <- expenses[,4]
+de <- expenses[,1]
+dm <- data.frame(de,md)
+
+
+ggplot(data = dm, aes(x = de, y = md))+
+  geom_point(color="blue",size = 4 )+
+  labs(x = "Expenses",
+       y = "Medical",
+       title = "Medical Expenses")
+
+#Distribution medical expenses - Histogram
+
+ggplot(expenses, aes(x= Medical)) +
+  geom_histogram(binwidth = 5,color="black",fill = "blue")
+
+# Minimum and Maximum
+
+tot<-expenses[,2:9]
+total<-colSums(tot)
+
+minvalue<-min(total)
+maxvalue<-max(total)
