2837b644da678dca4c0ee79149f83b0660cdb41c
Vikram S <vikram.senthil@outlook.com>
2021-01-27 12:08:35 +0530

    created pie chart

--- Week 9/Data Visualization.R
+++ Week 9/Data Visualization.R
@@ -8,9 +8,7 @@ expenses$Date <- as.Date(expenses$Date,
                          "%d/%m/%Y")
 str(expenses)
 
-pc<-read.csv("piechart.csv")
 
-View(pc)
 
 #Compare Expenses
 fd <-expenses[,2] #food column 
@@ -59,5 +57,17 @@ ggplot(data = dg, aes(x = dt, y = rs))+
   labs(x = "Date",
        y = "Expenses",
        title = "Total Expenses")
+
+#Pie Chart of Expenses
+pc<-read.csv("piechart.csv")
+
+View(pc)
+
+pie <-  ggplot(pc, aes(x = "", y=amount, fill = factor(expense))) +
+  geom_bar(width = 1, stat ="identity")+
+  labs(fill="expense",
+       x=NULL, y=NULL, title="Pie Chart of expenses")
+
+pie + coord_polar(theta = "y", start = 0)
 
 
