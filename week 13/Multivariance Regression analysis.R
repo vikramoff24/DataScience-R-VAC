22413242f72a36f150e76afe7e766943bb10deee
Vikram S <vikram.senthil@outlook.com>
2021-03-09 20:24:40 +0530

    Create Multivariance Regression analysis.R

---
+++ week 13/Multivariance Regression analysis.R
@@ -0,0 +1,24 @@
+dataset = read.csv("Admission_Predict.csv")
+data=dataset[,2:9]
+View(data)
+
+library(caTools)
+set.seed(123) 
+
+split = sample.split(data$Chance.of.Admit, SplitRatio = 0.8)
+View(split)
+
+training_set=subset(data,split==TRUE)
+testing_set=subset(data,split==FALSE)
+View(training_set)
+View(testing_set)
+
+regressor = lm(formula = Chance.of.Admit~., data = training_set)
+
+y_pred = predict(regressor, newdata = testing_set)
+
+a = testing_set$Chance.of.Admit
+b = y_pred
+
+plot(a,type='o',col='blue')
+lines(b,type='o',col='red')
